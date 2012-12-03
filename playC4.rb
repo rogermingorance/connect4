bbd9c711e82fb946627403ed6f8c11aeff43c9a4
Mingorance <rmingorance@ramdesk1.local>
2012-12-03 12:45:26 -0500

    master file w/ high level pseudo code

---
+++ playC4.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+
+
+
+
+=begin
+*High Level Pseudo code*
+instantiate a "c4" object
+
+--Display board
+loop:
+1) Accept input from user
+--Display board
+2) Verify if game is won
+3) Computer plays
+--Display board
+4) Vefify if game is won
+end loop
+=end
